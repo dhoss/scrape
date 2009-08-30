cad681fb58e9e9449b4d3b61c091bc70ada4db35
Devin Austin <dhoss@rebecca-kuris-macbook.local>
2009-08-30 04:49:09 -0600

    got robot to follow links

--- scrape.pl
+++ scrape.pl
@@ -4,21 +4,34 @@ use strict;
 use Web::Scraper;
 use URI;
 use Data::Dumper;
+use WWW::Mechanize;
 
-my $want = scraper {
-    process "li.gold", "contractors[]" => scraper { 
-        process ".omnitureListingNameLink",   name    => 'TEXT';
-        process ".address", address => 'TEXT'; # need to split this up into address, state, postcode,
-        process ".phoneNumber",               phone   => 'TEXT';
-        process ".links",                     website => 'TEXT';        
+
+my $base_url = "http://yellowpages.com.au/search/postSearchEntry.do?clueType=0&clue=electrical+contractors&locationClue=All+States&x=0&y=0";
+my $mech = WWW::Mechanize->new;
+print "mech object initiated\n";
+$mech->get( $base_url );
+print "got our url\n";
+my $names;
+
+print "Entering link following loop\n";
+while ( $mech->follow_link( text => "Next" ) ) {
+    print "Beginning scrape inside loop\n";
+    my $want = scraper {
+        process "li.gold", "contractors[]" => scraper { 
+            process ".omnitureListingNameLink",   name    => 'TEXT';
+            process ".address", address => 'TEXT'; # need to split this up into address, state, postcode,
+            process ".phoneNumber",               phone   => 'TEXT';
+            process ".links",                     website => 'TEXT';        
+        };
     };
-};
 
-my $names = $want->scrape( 
-    URI->new("http://yellowpages.com.au/search/postSearchEntry.do?clueType=0&clue=electrical+contractors&locationClue=All+States&x=0&y=0") 
-);
+    print "Before scrape is called\n";
+    $names = $want->scrape( 
+        URI->new($base_url) 
+    );
+    print "Scrape successful\n";
+}
 
 print Dumper($names);
-#for my $name (%{$names}) {
-#    print "Name: $name->{a_span}\n";
-#}
+
